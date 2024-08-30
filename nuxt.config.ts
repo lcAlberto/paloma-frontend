98cefc8c148421e89b275d37072180733ee3e137
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-08-30 18:25:17 -0300

    added primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,47 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  ssr: false,
-  devtools: { enabled: true },
-  app: {
-    head: {
-      link: [
-        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' }
-      ],
-      meta: [
-        { charset: 'utf-8' },
-        { 'data-theme': 'light' },
-        { name: 'csrf-token', content: '' }
-      ]
-    }
-  },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    compatibilityDate: '2024-04-03',
+    ssr: true,
+    devtools: {enabled: true},
+    app: {
+        head: {
+            link: [
+                {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'}
+            ],
+            meta: [
+                {charset: 'utf-8'},
+                {'data-theme': 'light'},
+                {name: 'csrf-token', content: ''}
+            ]
+        }
     },
-  },
+    css: [
+        '~/assets/css/main.css',
+        '~/assets/css/primevue-theme.css'
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    modules: [
+        '@primevue/nuxt-module'
+    ],
+    // primevue: {
+    //     options: {
+    //         theme: {
+    //             preset: Aura,
+    //             options: {
+    //                 // prefix: 'p',
+    //                 darkModeSelector: 'system',
+    //                 cssLayer: {
+    //                     name: 'primevue',
+    //                     order: 'tailwind-base, primevue, tailwind-utilities'
+    //                 }
+    //             }
+    //         }
+    //     }
+    // },
 })
